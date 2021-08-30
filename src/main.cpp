3a43af509bc18da3712bf96ea6f50522a7a2644e
Wolfgang Hoenig <whoenig@caltech.edu>
2021-08-30 16:11:33 +0200

    cleanup src folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,141 +1,116 @@
-#include <cstdlib>
-#include <cstring>
+//
+// receiver.cpp
+// ~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2019 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <array>
 #include <iostream>
+#include <string>
 #include <boost/asio.hpp>
+#include <inttypes.h>
+#include <stdio.h>
+
+constexpr const char* MULTICAST_ADDRESS = "239.255.42.99";
+constexpr int PORT_COMMAND = 1510;
+constexpr int PORT_DATA = 1511;
+constexpr int MAX_PACKETSIZE = 100000;  // max size of packet (actual packet size is dynamic)
+
+void Unpack(char* pData);
+void buildConnectPacket(std::vector<char>& buffer);
+void UnpackCommand(char* pData);
 
 using boost::asio::ip::udp;
 
-enum { max_length = 1024 };
-
-const char* MULTICAST_ADDRESS = "239.255.42.99";
-const int PORT_COMMAND = 1510;
-const int PORT_DATA = 1511;
-
-const int MAX_PACKETSIZE = 100000;  // max size of packet (actual packet size is dynamic)
-
-// typedef struct
-// {
-//   uint16_t iMessage;                // message ID (e.g. NAT_FRAMEOFDATA)
-//   uint16_t nDataBytes;              // Num bytes in payload
-//   void* data;
-// } sPacket;
-
-#define MAX_NAMELENGTH              256
-
-// sender
-typedef struct
+class receiver
 {
-    char szName[MAX_NAMELENGTH];            // sending app's name
-    unsigned char Version[4];               // sending app's version [major.minor.build.revision]
-    unsigned char NatNetVersion[4];         // sending app's NatNet version [major.minor.build.revision]
+public:
+  receiver(boost::asio::io_service& io_service,
+      const boost::asio::ip::address& listen_address,
+      const boost::asio::ip::address& multicast_address)
+    : socket_(io_service)
+    , sender_endpoint_()
+    , data_(20000)
+  {
+    // Create the socket so that multiple may be bound to the same address.
+    boost::asio::ip::udp::endpoint listen_endpoint(
+        listen_address, PORT_DATA);
+    socket_.open(listen_endpoint.protocol());
+    socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+    socket_.bind(listen_endpoint);
 
-} sSender;
+    // Join the multicast group.
+    socket_.set_option(
+        boost::asio::ip::multicast::join_group(multicast_address));
 
-typedef struct
-{
-    unsigned short iMessage;                // message ID (e.g. NAT_FRAMEOFDATA)
-    unsigned short nDataBytes;              // Num bytes in payload
-    union
-    {
-        unsigned char  cData[MAX_PACKETSIZE];
-        char           szData[MAX_PACKETSIZE];
-        unsigned long  lData[MAX_PACKETSIZE/4];
-        float          fData[MAX_PACKETSIZE/4];
-        sSender        Sender;
-    } Data;                                 // Payload incoming from NatNet Server
+    do_receive();
+  }
 
-} sPacket;
+private:
+  void do_receive()
+  {
+    socket_.async_receive_from(
+        boost::asio::buffer(data_.data(), data_.size()), sender_endpoint_,
+        [this](boost::system::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec)
+          {
+            // std::cout.write(data_.data(), length);
+            // std::cout << std::endl;
+            Unpack(data_.data());
 
-enum NatNetMessageID
-{
-  NAT_CONNECT              = 0,
-  NAT_SERVERINFO           = 1,
-  NAT_REQUEST              = 2,
-  NAT_RESPONSE             = 3,
-  NAT_REQUEST_MODELDEF     = 4,
-  NAT_MODELDEF             = 5,
-  NAT_REQUEST_FRAMEOFDATA  = 6,
-  NAT_FRAMEOFDATA          = 7,
-  NAT_MESSAGESTRING        = 8,
-  NAT_UNRECOGNIZED_REQUEST = 100,
+            do_receive();
+          } else {
+            std::cerr << "async_receive_from error: " << ec.message() << std::endl;
+          }
+        });
+  }
+
+  boost::asio::ip::udp::socket socket_;
+  boost::asio::ip::udp::endpoint sender_endpoint_;
+  std::vector<char> data_;
 };
 
 int main(int argc, char* argv[])
 {
   try
   {
+    // Connect to command port to query version
+
     if (argc != 2)
     {
-      std::cerr << "Usage: blocking_udp_echo_client <host>\n";
+      std::cerr << "Usage: natnettest <host>\n";
       return 1;
     }
 
-    boost::asio::io_service io_service;
+    boost::asio::io_service io_service_cmd;
 
-    udp::socket s(io_service, udp::endpoint(udp::v4(), 0));
+    udp::socket socket_cmd(io_service_cmd, udp::endpoint(udp::v4(), 0));
 
-    udp::resolver resolver(io_service);
-    udp::endpoint endpoint = *resolver.resolve({udp::v4(), argv[1], std::to_string(PORT_COMMAND)});
+    udp::resolver resolver_cmd(io_service_cmd);
+    udp::endpoint endpoint_cmd = *resolver_cmd.resolve({udp::v4(), argv[1], std::to_string(PORT_COMMAND)});
 
-    sPacket packet;
-    packet.iMessage = NAT_CONNECT;
-    packet.nDataBytes = 0;
-    s.send_to(boost::asio::buffer(&packet, 4), endpoint);
+    std::vector<char> connectCmd;
+    buildConnectPacket(connectCmd);
+    socket_cmd.send_to(boost::asio::buffer(connectCmd.data(), connectCmd.size()), endpoint_cmd);
 
-    std::vector<uint8_t> reply(MAX_PACKETSIZE);
+    std::vector<char> reply(MAX_PACKETSIZE);
     udp::endpoint sender_endpoint;
-    size_t reply_length = s.receive_from(
+    /*size_t reply_length =*/ socket_cmd.receive_from(
         boost::asio::buffer(reply, MAX_PACKETSIZE), sender_endpoint);
 
-    const sPacket* replyPacket = reinterpret_cast<const sPacket*>(reply.data());
-    std::cout << replyPacket->iMessage << " " << replyPacket->nDataBytes << std::endl;
+    UnpackCommand(reply.data());
 
-
-
-    int NatNetVersion[4] = {0,0,0,0};
-    int ServerVersion[4] = {0,0,0,0};
-
-
-    // handle command
-        switch (replyPacket->iMessage)
-        {
-        // case NAT_MODELDEF:
-        //     Unpack((char*)&PacketIn);
-        //     break;
-        // case NAT_FRAMEOFDATA:
-        //     Unpack((char*)&PacketIn);
-        //     break;
-        case NAT_SERVERINFO:
-            for(int i=0; i<4; i++)
-            {
-                NatNetVersion[i] = (int)replyPacket->Data.Sender.NatNetVersion[i];
-                ServerVersion[i] = (int)replyPacket->Data.Sender.Version[i];
-            }
-            break;
-        // case NAT_RESPONSE:
-        //     gCommandResponseSize = PacketIn.nDataBytes;
-        //     if(gCommandResponseSize==4)
-        //         memcpy(&gCommandResponse, &PacketIn.Data.lData[0], gCommandResponseSize);
-        //     else
-        //     {
-        //         memcpy(&gCommandResponseString[0], &PacketIn.Data.cData[0], gCommandResponseSize);
-        //         printf("Response : %s", gCommandResponseString);
-        //         gCommandResponse = 0;   // ok
-        //     }
-        //     break;
-        // case NAT_UNRECOGNIZED_REQUEST:
-        //     printf("[Client] received 'unrecognized request'\n");
-        //     gCommandResponseSize = 0;
-        //     gCommandResponse = 1;       // err
-        //     break;
-        // case NAT_MESSAGESTRING:
-        //     printf("[Client] Received message: %s\n", PacketIn.Data.szData);
-        //     break;
-        }
-
-    std::cout << "NatNetVersion: " << NatNetVersion[0] << "." << NatNetVersion[1] << "." << NatNetVersion[2] << "." << NatNetVersion[3] << std::endl;
-    std::cout << "ServerVersion: " << ServerVersion[0] << "." << ServerVersion[1] << "." << ServerVersion[2] << "." << ServerVersion[3] << std::endl;
-
+    // Listen on multicast address
+    boost::asio::io_service io_service;
+    receiver r(io_service,
+        boost::asio::ip::address::from_string("0.0.0.0"),
+        boost::asio::ip::address::from_string(MULTICAST_ADDRESS));
+    io_service.run();
   }
   catch (std::exception& e)
   {
